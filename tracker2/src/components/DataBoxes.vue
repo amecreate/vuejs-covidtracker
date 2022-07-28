3a8f2c2ec99283a01ce35cb3c6dcf4ade190f438
amecreate <amelieasmile@gmail.com>
2022-07-28 15:44:54 -0400

    add second box to display death data

--- tracker2/src/components/DataBoxes.vue
+++ tracker2/src/components/DataBoxes.vue
@@ -1,15 +1,31 @@
 <template>
-    <div class="grid md:grid-cols-2 gap-4"></div>
-    <div class="shadow-md bg-blue-100 p-10 text-center">
-        <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+    <div class="grid md:grid-cols-2 gap-4">
+        <!-- Box 1 -->
+        <div class="shadow-md bg-blue-100 p-10 text-center">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
 
-        <div class="text-2xl mb-4">
-            <span class="font-bold">New:</span>
-            {{ stats.NewConfirmed }}
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ stats.NewConfirmed }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{ stats.TotalConfirmed }}
+            </div>
         </div>
-        <div class="text-2xl mb-4">
-            <span class="font-bold">Total:</span>
-            {{ stats.TotalConfirmed }}
+
+        <!-- Box 2 -->
+            <div class="shadow-md bg-blue-200 p-10 text-center">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Deaths:</span>
+                {{ stats.NewDeaths }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{ stats.TotalDeaths }}
+            </div>
         </div>
     </div>
 </template>
