bcb1a0ac66bce04fcc1f2d69e6a5fb8af807b2d8
amecreate <amelieasmile@gmail.com>
2022-07-28 15:40:16 -0400

    add columns to DataBoxes. add props stats.NewConfirmed

--- tracker2/src/components/DataBoxes.vue
+++ tracker2/src/components/DataBoxes.vue
@@ -1,5 +1,13 @@
 <template>
-    Data Boxes
+    <div class="grid md:grid-cols-2 gap-4"></div>
+    <div class="shadow-md bg-blue-100 p-10 text-center">
+        <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+
+        <div class="text-2xl mb-4">
+            <span class="font-bold">New:</span>
+            {{ stats.NewConfirmed }}
+        </div>
+    </div>
 </template>
 
 <script>
