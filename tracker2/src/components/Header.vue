d4c3923730f09ec7b66a03ed494fde6ea1636a76
amecreate <amelieasmile@gmail.com>
2022-07-28 14:27:51 -0400

    fetch data from api in HomeView.vue. Use the lifecycle method created to run the fetch method

--- tracker2/src/components/Header.vue
+++ tracker2/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i> Tracker</div>
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i> Covid Tracker</div>
 
     <p>API BY <a class="text-blue-300" href="https://covid19api.com" target="_blank">covid19api.com</a></p>
   </header>
