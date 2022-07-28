ede4b64e8f09c02e3fceeeaadf21e661c151c9d3
amecreate <amelieasmile@gmail.com>
2022-07-28 16:40:19 -0400

    add click event to button. the event will reset the title and stats and re-fetch the data from api

--- tracker2/src/views/HomeView.vue
+++ tracker2/src/views/HomeView.vue
@@ -3,7 +3,7 @@
   <DataTitle :text="title" :dataDate="dataDate"/>
   <DataBoxes :stats="stats" />
   <CountrySelect @get-country="getCountryData" :countries="countries" />
-  <button v-if="stats.Country" class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">
+  <button @click="clearCountryData" v-if="stats.Country" class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">
     Clear Country
   </button>
 
@@ -47,6 +47,14 @@ export default {
     getCountryData(country) {
       this.stats = country
       this.title = country.Country
+    },
+
+    async clearCountryData() {
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
     }
   },
   async created() {
