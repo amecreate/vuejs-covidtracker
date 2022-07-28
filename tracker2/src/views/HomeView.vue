8ba1d468073fc7f0d6450a14c894ccb5a86291be
amecreate <amelieasmile@gmail.com>
2022-07-28 14:42:15 -0400

    add v-else to display a text while fetching data

--- tracker2/src/views/HomeView.vue
+++ tracker2/src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <template>
-<div class="home">
+<main v-if="!loading">
+  Show Data
+</main>
 
-  HelloWorld
-</div>
+<main class="flex flex-col align-center justify-center text-center" v-else>
+  <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+  
+</main>
 </template>
 
 <script>
