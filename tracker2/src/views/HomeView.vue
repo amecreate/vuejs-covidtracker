fac0da4ab7f0dabb4c4329d8ce68059763194db4
amecreate <amelieasmile@gmail.com>
2022-07-28 14:43:51 -0400

    add image binding

--- tracker2/src/views/HomeView.vue
+++ tracker2/src/views/HomeView.vue
@@ -5,7 +5,7 @@
 
 <main class="flex flex-col align-center justify-center text-center" v-else>
   <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
-  
+  <img :src="loadingImage" class="w-24 m-auto" alt="">
 </main>
 </template>
 
