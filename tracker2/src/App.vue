8b19947613540f2c1bd7213b14d1a73dee60ac76
amecreate <amelieasmile@gmail.com>
2022-07-28 13:53:51 -0400

    add Header to App.vue

--- tracker2/src/App.vue
+++ tracker2/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="container">
-    <router-view/>
-  </div>
+  <Header />
+  <router-view/>
+  
 </template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
